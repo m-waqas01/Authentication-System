d5ebe15c3835229c5f0c08965983cb258dd89284
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-10-26 10:06:22 +0000

    get users updated now

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,5 +11,10 @@ const router = express.Router();
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.get("/verify", authMiddleware, verifyToken);
+// In authRoutes.js
+router.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
 
 export default router;
