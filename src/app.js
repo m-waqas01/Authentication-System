5eb7ef879e36eedc3b07fe9dc634825bf7632939
Muhammad Waqas <muhammadwaqas538039@gmail.com>
2025-10-26 10:00:44 +0000

    Routes updated now

--- src/app.js
+++ src/app.js
@@ -1,41 +1,21 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/authRoutes.js";
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/auth", authRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("✅ MERN Auth Backend is Running!");
-// });
-
-// export default app;
-
-// backend/src/app.js
 import express from "express";
-import connectDB from "./configuration/dbconfig.js";
+import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import mongoose from "mongoose";
 
 dotenv.config();
-
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-// Connect to DB
-connectDB();
-
-// ✅ Mount routes
 app.use("/api/auth", authRoutes);
 
-// ✅ Test Route to Check DB Status
-import mongoose from "mongoose";
+app.get("/", (req, res) => {
+  res.send("✅ MERN Auth Backend is Running!");
+});
+
 app.get("/test-db", (req, res) => {
   const dbState = mongoose.connection.readyState;
   const states = ["disconnected", "connected", "connecting", "disconnecting"];
